671811574685dc073e988729585310187c2a57db
yuseonpark <pyuseon@gmail.com>
2022-02-07 23:08:15 +0900

    순한맛 완료

--- 03_reataurant/src/main/java/com/example/reataurant/controller/OrderController.java
+++ 03_reataurant/src/main/java/com/example/reataurant/controller/OrderController.java
@@ -1,43 +1,4 @@
 package com.example.reataurant.controller;
 
-
-import com.example.reataurant.dto.DetailsRequestDto;
-import com.example.reataurant.dto.OrderRequestDto;
-import com.example.reataurant.dto.OrderResponseDto;
-import com.example.reataurant.model.Orders;
-import com.example.reataurant.service.OrderService;
-import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
-
-@RestController
-@RequiredArgsConstructor
 public class OrderController {
-
-    private final OrderService orderService;
-
-//    @Autowired
-//    public OrderController(OrderService orderService) {
-//        this.orderService = orderService;
-//    }
-
-
-    @PostMapping("/order/request")
-    public OrderResponseDto addOrders(@RequestBody OrderRequestDto requestDto) throws Exception {
-        {
-            Long restaurantId = requestDto.getRestaurantId();
-            List<DetailsRequestDto> orderListDto = requestDto.getFoods();
-            return  orderService.addOrders(orderListDto, restaurantId);
-        }
-    }
-
-    @GetMapping("/orders")
-    public List<Orders> getOrders()
-    {
-        return orderService.getOrders();
-    }
 }
--- 03_reataurant/src/main/java/com/example/reataurant/repository/RestaurantRepository.java
+++ 03_reataurant/src/main/java/com/example/reataurant/repository/RestaurantRepository.java
@@ -4,9 +4,7 @@ import com.example.reataurant.model.Restaurant;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
-import java.util.Optional;
 
 public interface RestaurantRepository extends JpaRepository<Restaurant,Long> {
     List<Restaurant> findAll();
-    Optional<Restaurant> findById(Long Id);
 }
